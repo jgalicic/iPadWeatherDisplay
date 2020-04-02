79518168abed152cc27acba86cb6eae675c7107f
Justin Galicic <jgalicic@Justins-iMac.hgate.us>
2020-04-01 17:16:07 -0700

    removed jQuery

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-$(document).ready(function() {
-  const monthNames = [
-    "Jan",
-    "Feb",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "Aug",
-    "Sept",
-    "Oct",
-    "Nov",
-    "Dec"
-  ]
-  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-  const dayOfWeek = document.getElementById("dayOfWeek")
-  const todaysDate = document.getElementById("todayDate")
-  const time = document.getElementById("time")
-  const currentTemp = document.getElementById("currentTemp")
-  const currentCond = document.getElementById("currentCond")
-  const forecast = document.getElementById("forecast")
+const monthNames = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec"
+]
+const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+const dayOfWeek = document.getElementById("dayOfWeek")
+const todaysDate = document.getElementById("todayDate")
+const time = document.getElementById("time")
+// const currentTemp = document.getElementById("currentTemp")
+// const currentCond = document.getElementById("currentCond")
+// const forecast = document.getElementById("forecast")
 
-  setInterval(function() {
-    let date = new Date()
-    dayOfWeek.innerText = dayNames[date.getDay()]
-    todaysDate.innerText = `${
-      monthNames[date.getMonth()]
-    } ${date.getDate()}, ${date.getFullYear()}`
-    time.innerText = date.toLocaleTimeString().match(/[0-9]+[:][0-9]+/g)
-  }, 500)
-})
+setInterval(function() {
+  let date = new Date()
+  dayOfWeek.innerText = dayNames[date.getDay()]
+  todaysDate.innerText = `${
+    monthNames[date.getMonth()]
+  } ${date.getDate()}, ${date.getFullYear()}`
+  time.innerText = date.toLocaleTimeString().match(/[0-9]+[:][0-9]+/g)
+}, 500)
