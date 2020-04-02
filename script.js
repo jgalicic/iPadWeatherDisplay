b743e28e7d14ab35ddff5191a136169bf24fd405
Justin Galicic <jgalicic@Justins-iMac.hgate.us>
2020-04-01 19:36:54 -0700

    added pics

--- script.js
+++ script.js
@@ -27,12 +27,15 @@ setInterval(function() {
     monthNames[date.getMonth()]
   } ${date.getDate()}, ${date.getFullYear()}`
   time.innerText = date.toLocaleTimeString().match(/[0-9]+[:][0-9]+/g)
+
+  // Background images
+  let img
+  if (date.getMinutes() >= 0 && date.getMinutes() < 10) img = "img-1"
+  if (date.getMinutes() >= 10 && date.getMinutes() < 20) img = "img-2"
+  if (date.getMinutes() >= 20 && date.getMinutes() < 30) img = "img-3"
+  if (date.getMinutes() >= 30 && date.getMinutes() < 40) img = "img-4"
+  if (date.getMinutes() >= 40 && date.getMinutes() < 50) img = "img-5"
+  if (date.getMinutes() >= 50 && date.getMinutes() < 61) img = "img-6"
+
+  document.body.style.backgroundImage = `url("img/${img}.jpg")`
 }, 500)
-
-// const date = new Date()
-
-// dayOfWeek.innerText = dayNames[date.getDay()]
-// todaysDate.innerText = `${
-//   monthNames[date.getMonth()]
-// } ${date.getDate()}, ${date.getFullYear()}`
-// time.innerText = date.toLocaleTimeString().match(/[0-9]+[:][0-9]+/g)
